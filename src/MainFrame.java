50db2f9cad0598b0d53044003c8b3f1209e9379f
JakeCarduck <139373612+JakeCarduck@users.noreply.github.com>
2025-01-30 19:23:25 -0500

    Update MainFrame.java

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -4,18 +4,19 @@ public class MainFrame {
     private static int numGoodDeeds = 0;
     private static int numBadDeeds = 0;
     private static int worldSustainability = 0;
-    private final static int worldSustainabilityPerPercent = 1.5; // max for bar is 150 points
+    private final static double worldSustainabilityPerPercent = 1.5; // max for bar is 150 points
 
 
     /**
-     *  Creates a visual bar to represent the world sustainability using the static variable
-     *   world sustainability as an input.
+     * Creates a visual bar to represent the world sustainability using the static variable
+     * world sustainability as an input.
+     *
      * @return a progress bar of the world sustainability as a String
      */
     private static String createSustainabilityBar() {
         String worldSustainabilityBar = "[";
         // progress bar progression: each "=" represents 5%
-        int progress = worldSustainability / (worldSustainabilityPerPercent * 5);
+        double progress = worldSustainability / (worldSustainabilityPerPercent * 5);
         if (progress > 20) { // can have a score higher than 150, but the bar won't overfill
             progress = 20;
         }
@@ -32,9 +33,9 @@ public class MainFrame {
     }
 
     /**
-     *  Provides general background information about Pretty Planet's setting, as well as
-     *   information regarding the player's number of good and bad deeds, the world's sustainability
-     *   progress, the player's sustainability score, and options to resume the game or quit it.
+     * Provides general background information about Pretty Planet's setting, as well as
+     * information regarding the player's number of good and bad deeds, the world's sustainability
+     * progress, the player's sustainability score, and options to resume the game or quit it.
      */
     public static void menu() {
         // background information
@@ -70,170 +71,167 @@ public class MainFrame {
 
     public static void main(String[] args) {
         worldSustainability = 51;
-	    int userChoice = 0;
-		//int progress = checkStatus(); use this method to check on status
-
-
-	    System.out.print("Hi there! Your planet is currently improving, but there is still work to be done in your day to day...\n");
-        Scanner in = new Scanner(System.in);
         int userChoice = 0;
+        //int progress = checkStatus(); use this method to check on status
+
+
+        System.out.print("Hi there! Your planet is currently improving, but there is still work to be done in your day to day...\n");
+        Scanner in = new Scanner(System.in);
         while (userChoice != 1 && userChoice != 2 && userChoice != 3) {
             System.out.print("Wow look at the time! You almost missed your doctors appointment. Would you like to walk (1), ride the bike (2), or drive (3)?\n");
             userChoice = in.nextInt();
 
-            if (worldSustainability <= 75  && worldSustainability > 100){
-            if (userChoice == 1 || userChoice == 2) {
-                System.out.print("You just repaired a part of your Ozone layer. Now there's less smog and your planet is prettier!\n");
-                worldSustainibility += 30; 
-                numGoodDeeds++;
-            } else if (userChoice == 3) {
-                System.out.print("You drove your car. There is now extreme air pollution making it difficult to breathe.\n");
-                numBadDeeds++;
-            } else {
-                System.out.print("Error. You must choose either 1,2, or 3.\n");
-            }
-                
-        }
-
-
-        while (worldSustainability <= 150) {
-            System.out.print("Pick an option to better the environment(1) or stop playing(2)!/n");
-            int userSecChoice = in.nextInt();
-            if (userSecChoice == 1) {
-                while (true) {
-                    if(worldSustainability <= 50 && worldSustainability > 75){
-                    System.out.print("Looks like you are thirsty! Buy a 40 pack of plastic water bottles(1) or drink fridge water(2)?\n");
-                    int userThirdChoice = in.nextInt();
-
-                    if (userThirdChoice == 1) {
-                        System.out.print("Unfortunately your water bottles were not properly disposed of. The ocean now has over 70% of plastic waste in it making it difficult for species to survive.\n");
-                        numBadDeeds;
-                        break;
-                    } else if (userThirdChoice == 2) {
-                        System.out.print("You just contributed to less plastic waste. There is less garbage in the ocean now! Your planet is prettier! \n");
-                        worldSustainability += 20;
-                        numGoodDeeds++;
-                        break;
-                    } else {
-                        System.out.print("Error. You must choose either 1 or 2.\n");
-                    }
-                    }
+            if (worldSustainability <= 75 && worldSustainability > 100) {
+                if (userChoice == 1 || userChoice == 2) {
+                    System.out.print("You just repaired a part of your Ozone layer. Now there's less smog and your planet is prettier!\n");
+                    worldSustainability += 30;
+                    numGoodDeeds++;
+                } else if (userChoice == 3) {
+                    System.out.print("You drove your car. There is now extreme air pollution making it difficult to breathe.\n");
+                    numBadDeeds++;
+                } else {
+                    System.out.print("Error. You must choose either 1,2, or 3.\n");
                 }
 
-            } else if (userSecChoice == 2) {
-                System.out.print("Thank you for playing! Goodbye!");
-                break;
-            } else {
-                System.out.print("Error. You must choose either 1 or 2.");
             }
-	}
-		//when progress perct less than 50 
-		if (worldSustainability <= 50 && worldSustainability > 30) {
-			System.out.println("Planet isn't looking good friend!");
-			System.out.println();
-			System.out.println("Do you want to?..");
-			System.out.println("      1. Turn on your sprinklers and water all your flowers?");
-			System.out.println("Or    2. Don't water the lawn at all, it's raining tomorrow.");
-			
-			System.out.println("Type your option below ( 1 ) or ( 2 ) ");
-			System.out.println();
-			userChoice = in.nextInt();
-						
-						while (userChoice != 3) {
-							switch (userChoice) {
-							case 1:
-								System.out.println();
-								System.out.println("Why not save that water for firefighters? -15%!!");
-								worldSustainability -= 15;
-								numBadDeeds += 1;
-								break;
-							case 2:
-								System.out.println();
-								System.out.println("Great work! Waiting for a rainy day is best for your wallet and the planet! +10%");
-								worldSustainability += 10;
-								numGoodDeeds += 1;
-								break;
-							default:
-								System.out.println("Invalid option. Please enter [1 or 2]");
-								break;
-							}
-						
-							System.out.println();
-							userChoice = in.nextInt();
-						}
-			}
-		else if (worldSustainability <= 30 && worldSustainability > 20) {
-			System.out.println("Planet getting a little scary!");
-			System.out.println();
-			System.out.println("Do you want to?..");
-			System.out.println("      1. Tidy up the storm basins (drains)?");
-			System.out.println("Or    2. Throw away trash in public places");
-			
-			System.out.println();
-			System.out.println();
-			System.out.println("Type your option below ( 1 ) or ( 2 ) ");
-			System.out.println();
-			userChoice = in.nextInt();
-						
-						while (userChoice != 2) {
-							switch (userChoice) {
-							case 1:
-								System.out.println();
-								System.out.println("This will help keep public water clean and maybe help a nearby critter! +20%");
-								worldSustainability += 20;
-								numGoodDeeds += 1;
-								break;
-							case 2:
-								System.out.println();
-								System.out.println("Come on litter bug! That's not pretty at all! -25%");
-								worldSustainability -= 25;
-								numBadDeeds += 1;
-								break;
-							default:
-								System.out.println("Invalid option. Please enter [1 or 2]");
-								break;
-							}
-						
-							System.out.println();
-							userChoice = in.nextInt();
-						}
-			}
-		
-		else if (worldSustainability <= 20 && worldSustainability > 10) {
-			System.out.println("Planet is on its way out, We gotta save it!!");
-			System.out.println();
-			System.out.println("Do you want to?..");
-			System.out.println("      1. Throw away electronics improperly?");
-			System.out.println("Or    2. Recycle your old electronics and batteries");
-			
-			System.out.println();
-			System.out.println();
-			System.out.println("Type your option below ( 1 ) or ( 2 ) ");
-			System.out.println();
-			userChoice = in.nextInt();
-						
-						while (userChoice != 2) {
-							switch (userChoice) {
-							case 1:
-								System.out.println();
-								System.out.println("Now there is acid in the soil you jerk! -30%");
-								worldSustainability -= 30;
-								numBadDeeds += 1;
-								break;
-							case 2:
-								System.out.println();
-								System.out.println("Nice work, now those wires and hardware can be re-used! +30%");
-								worldSustainability += 30;
-								numGoodDeeds += 1;
-								break;
-							default:
-								System.out.println("Invalid option. Please enter [1 or 2]");
-								break;
-							}
-						
-							System.out.println();
-							userChoice = in.nextInt();
-						}
-			}
+
+
+            while (worldSustainability <= 150) {
+                System.out.print("Pick an option to better the environment(1) or stop playing(2)!/n");
+                int userSecChoice = in.nextInt();
+                if (userSecChoice == 1) {
+                    while (true) {
+                        if (worldSustainability <= 50 && worldSustainability > 75) {
+                            System.out.print("Looks like you are thirsty! Buy a 40 pack of plastic water bottles(1) or drink fridge water(2)?\n");
+                            int userThirdChoice = in.nextInt();
+
+                            if (userThirdChoice == 1) {
+                                System.out.print("Unfortunately your water bottles were not properly disposed of. The ocean now has over 70% of plastic waste in it making it difficult for species to survive.\n");
+                                numBadDeeds++;
+                                break;
+                            } else if (userThirdChoice == 2) {
+                                System.out.print("You just contributed to less plastic waste. There is less garbage in the ocean now! Your planet is prettier! \n");
+                                worldSustainability += 20;
+                                numGoodDeeds++;
+                                break;
+                            } else {
+                                System.out.print("Error. You must choose either 1 or 2.\n");
+                            }
+                        }
+                    }
+
+                } else if (userSecChoice == 2) {
+                    System.out.print("Thank you for playing! Goodbye!");
+                    break;
+                } else {
+                    System.out.print("Error. You must choose either 1 or 2.");
+                }
+            }
+            //when progress perct less than 50
+            if (worldSustainability <= 50 && worldSustainability > 30) {
+                System.out.println("Planet isn't looking good friend!");
+                System.out.println();
+                System.out.println("Do you want to?..");
+                System.out.println("      1. Turn on your sprinklers and water all your flowers?");
+                System.out.println("Or    2. Don't water the lawn at all, it's raining tomorrow.");
+
+                System.out.println("Type your option below ( 1 ) or ( 2 ) ");
+                System.out.println();
+                userChoice = in.nextInt();
+
+                while (userChoice != 3) {
+                    switch (userChoice) {
+                        case 1:
+                            System.out.println();
+                            System.out.println("Why not save that water for firefighters? -15%!!");
+                            worldSustainability -= 15;
+                            numBadDeeds += 1;
+                            break;
+                        case 2:
+                            System.out.println();
+                            System.out.println("Great work! Waiting for a rainy day is best for your wallet and the planet! +10%");
+                            worldSustainability += 10;
+                            numGoodDeeds += 1;
+                            break;
+                        default:
+                            System.out.println("Invalid option. Please enter [1 or 2]");
+                            break;
+                    }
+
+                    System.out.println();
+                    userChoice = in.nextInt();
+                }
+            } else if (worldSustainability <= 30 && worldSustainability > 20) {
+                System.out.println("Planet getting a little scary!");
+                System.out.println();
+                System.out.println("Do you want to?..");
+                System.out.println("      1. Tidy up the storm basins (drains)?");
+                System.out.println("Or    2. Throw away trash in public places");
+
+                System.out.println();
+                System.out.println();
+                System.out.println("Type your option below ( 1 ) or ( 2 ) ");
+                System.out.println();
+                userChoice = in.nextInt();
+
+                while (userChoice != 2) {
+                    switch (userChoice) {
+                        case 1:
+                            System.out.println();
+                            System.out.println("This will help keep public water clean and maybe help a nearby critter! +20%");
+                            worldSustainability += 20;
+                            numGoodDeeds += 1;
+                            break;
+                        case 2:
+                            System.out.println();
+                            System.out.println("Come on litter bug! That's not pretty at all! -25%");
+                            worldSustainability -= 25;
+                            numBadDeeds += 1;
+                            break;
+                        default:
+                            System.out.println("Invalid option. Please enter [1 or 2]");
+                            break;
+                    }
+
+                    System.out.println();
+                    userChoice = in.nextInt();
+                }
+            } else if (worldSustainability <= 20 && worldSustainability > 10) {
+                System.out.println("Planet is on its way out, We gotta save it!!");
+                System.out.println();
+                System.out.println("Do you want to?..");
+                System.out.println("      1. Throw away electronics improperly?");
+                System.out.println("Or    2. Recycle your old electronics and batteries");
+
+                System.out.println();
+                System.out.println();
+                System.out.println("Type your option below ( 1 ) or ( 2 ) ");
+                System.out.println();
+                userChoice = in.nextInt();
+
+                while (userChoice != 2) {
+                    switch (userChoice) {
+                        case 1:
+                            System.out.println();
+                            System.out.println("Now there is acid in the soil you jerk! -30%");
+                            worldSustainability -= 30;
+                            numBadDeeds += 1;
+                            break;
+                        case 2:
+                            System.out.println();
+                            System.out.println("Nice work, now those wires and hardware can be re-used! +30%");
+                            worldSustainability += 30;
+                            numGoodDeeds += 1;
+                            break;
+                        default:
+                            System.out.println("Invalid option. Please enter [1 or 2]");
+                            break;
+                    }
+
+                    System.out.println();
+                    userChoice = in.nextInt();
+                }
+            }
+        }
     }
 }
